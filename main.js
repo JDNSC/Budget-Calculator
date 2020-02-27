a790402a9b39a899b5c27f9f7dae9aa260cd9c29
Jaden Cox <61036514+JDNSC@users.noreply.github.com>
2020-02-26 18:18:07 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+{var total = window.prompt("What is your total");
+document.write(total);
+var wants = total * .30; 
+var needs = total * .50;
+var savings = total * .20;
+
+}
+console.log(wants, needs, savings);
+
+
+
